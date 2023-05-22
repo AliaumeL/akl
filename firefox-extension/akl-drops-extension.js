549813c8a42fb583e871f7d49fe899f37ee233f8
Aliaume LOPEZ <aliaume.lopez@lsv.fr>
2023-05-22 17:16:44 +0200

    ✨ add support for DROPS of dagstuhl

---
+++ firefox-extension/akl-drops-extension.js
@@ -0,0 +1,64 @@
+setTimeout(() => {
+  "use strict";
+
+  const meta = (field) =>
+    document.querySelector(`meta[name="${field}"]`)?.content;
+
+  const DOI = meta("citation_doi") || meta("DC.identifier");
+  const publisher =
+    meta("citation_journal_title") || meta("citation_conference_title");
+
+  const date = meta("citation_date");
+  const year = parseInt(date.substring(0, 4));
+
+  const citation_url = meta("citation_pdf_url");
+  const title = meta("citation_title");
+
+  const authors = Array.from(
+    document.querySelectorAll("meta[name='citation_author']")
+  ).map((x) => x.content);
+
+  const identifiers = [
+    `https://dx.doi.org/${DOI}`,
+    `doi:${DOI}`,
+    window.location.href,
+  ];
+  const context = [];
+  if (publisher) {
+    context.push(publisher);
+  }
+
+  const payload = {
+    uri: citation_url,
+    title,
+    year,
+    authors,
+    identifiers,
+    context,
+    view: true,
+    force: false,
+  };
+
+  console.log(payload);
+
+  const query = new URLSearchParams({
+    payload: JSON.stringify(payload),
+  });
+
+  const url = "akl://import-document/?" + query.toString();
+
+  const table = document.querySelectorAll("tbody")[0];
+
+  const link = document.createElement("a");
+  const tr = document.createElement("tr");
+  const dlTitle = document.createElement("td");
+  const dlUrl = document.createElement("td");
+
+  link.innerHTML = "IMPORT DOCUMENT";
+  link.href = url;
+  dlTitle.innerHTML = "akl import:";
+  dlUrl.append(link);
+  tr.appendChild(dlTitle);
+  tr.appendChild(dlUrl);
+  table.appendChild(tr);
+}, 10);
