692a333990c7f3c852d40ec2f65ac77c3bdaba1a
Aliaume LOPEZ <aliaume.lopez@lsv.fr>
2023-07-13 17:00:50 +0200

    Better user interaction for the plugin

--- vim-plugin/akltex.py
+++ vim-plugin/akltex.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import sys
+import re
 import pathlib
 import bibtexparser
 import subprocess
-from typing import List, Dict, Optional
-
+import clipboard
+from urllib.parse import urlparse, parse_qs
+from typing import List, Dict, Optional, Tuple
 
 def find_citations(tex_root : pathlib.Path) -> List[Dict]:
     citations = []
@@ -14,84 +16,118 @@ def find_citations(tex_root : pathlib.Path) -> List[Dict]:
             citations.extend(bib_database.entries)
     return citations
 
-
-def select_citation(citations : List[Dict]) -> Optional[str]:
-    dmenu_input = []
-    for citation in citations:
-        ID  = citation.get("ID", None)
-        title   = citation.get("title", citation.get("booktitle", "-"))
-        authors = citation.get("author", "-")
-        year    = citation.get("year", "-")
-        title = title[:30]
-        dmenu_input.append(f"{ID}\t{year}\t{title}\t{authors}")
-    options = '\n'.join(option.replace('\n', ' ') for option in dmenu_input)
-    cmd     = ["rofi",
-               '-dmenu', '-p', "Select Citation", '-format', 's', '-i',
-               '-lines', '5']
-    result = subprocess.run(cmd, input=options, 
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.DEVNULL,
-                    universal_newlines=True)
-
-    if result.returncode == 0:
-        stdout = result.stdout.strip()
-        return stdout[:stdout.index("\t")]
-    else:
-        return None
-
-
 def open_citation():
     tex_root  = pathlib.Path(sys.argv[2])
+    bibkey    = pathlib.Path(sys.argv[3])
     citations = find_citations(tex_root)
 
-    filtered_citations_url = {}
-    filtered_citations = []
     for citation in citations:
-        ID  = citation.get("ID", None)
-        DOI = citation.get("DOI", None) or citation.get("doi",None)
-        URL = citation.get("url", None) or citation.get("URL",None)
-        link = URL or (DOI and f"https://dx.doi.org/{DOI}")
-        if ID and link:
-            filtered_citations_url[ID] = link
-            filtered_citations.append(citation)
+        if citation["ID"] == bibkey:
+            DOI = citation.get("DOI", None) or citation.get("doi",None)
+            URL = citation.get("url", None) or citation.get("URL",None)
+            arxiv = citation.get("eprint", None)
+            link = (URL or
+                    (DOI and f"https://dx.doi.org/{DOI}") or
+                    (arxiv and f"arxiv:{arxiv}"))
+            if link is not None:
+                subprocess.Popen(["akl", "open", "--uri", link],
+                            stderr=subprocess.DEVNULL,
+                            stdout=subprocess.DEVNULL)
+            return None
 
-    citation_id = select_citation(filtered_citations)
-    if citation_id is not None:
-        selected_url = filtered_citations_url[citation_id]
-        print(citation_id,end="")
-        subprocess.Popen(["akl", "open", "--uri", selected_url],
-                    stderr=subprocess.DEVNULL,
-                    stdout=subprocess.DEVNULL)
-    else:
-        print("Error")
+def citation_identities(citation: Dict) -> List[str]:
+    return [ citation[x] for x in ["DOI", "doi", "url", "URL", "eprint", "arxiv"]
+            if x in citation ]
+
+def citation_matches(citation: Dict, path : str) -> bool:
+    return any((x in path) or (path in x)
+            for x in citation_identities(citation))
+
+
+def create_kl_citation(citation: Dict, url : str, dest : Optional[str], page: Optional[str]) -> str:
+    ID = citation["ID"]
+    name = dest if dest is not None else page
+    return f"""
+\\knowledge{{url={{{url}}},bibkey={{{ID}}}}}
+  | {name}@{ID}"""
+
+
+def parse_kl_citations(file : pathlib.Path) -> Dict:
+    regdef = r"\\knowledge{url={(.+)},\s*bibkey={(.+)}}"
+    regsta = r"\s+\|\s+([^@]+)"
+    result = {}
+    current_key = None
+    current_url = None
+    with open(file,"r") as f:
+        for line in f.readlines():
+            x = re.search(regdef, line)
+            y = re.match(regsta, line)
+            if x is not None:
+                current_url = x.group(1).strip()
+                current_key = x.group(2).strip()
+            elif y is not None:
+                result[current_url] = {
+                        "key": current_key,
+                        "tag": y.group(1).strip()
+                }
+                current_key = None
+                current_url = None
+            else:
+                current_key = None
+                current_url = None
+    return result
+
+
 
 def create_citation():
-    ID   = sys.argv[2]
-    name = sys.argv[3]
-    url  = sys.argv[4]
-    print("\\akldef{")
-    print(f"\tkey={ID},")
-    print(f"name={name},")
-    print(f"url={url}")
-    print("}")
-
-def insert_citation():
     tex_root  = pathlib.Path(sys.argv[2])
-    citations = find_citations(tex_root)
+    if len(sys.argv) >= 4:
+        kl_file   = pathlib.Path(sys.argv[3])
+    else:
+        kl_file = tex_root / "knowledges.tex"
+        kl_file.touch(exist_ok=True)
 
-    citation_id = select_citation(citations)
-    if citation_id is not None:
-        print(citation_id.strip(),end="")
+    pasted_url = clipboard.paste()
+
+    quotes = parse_kl_citations(kl_file)
+    if pasted_url in quotes:
+        quote = quotes[pasted_url]
+        print("\\cite["+quote["tag"]+"]{"+quote["key"]+"}",end="")
+    else:
+        citations = find_citations(tex_root)
+        parsed = urlparse(pasted_url)
+        query   = parse_qs(parsed.query)
+
+        dest = query.get("dest",[])
+        dest = dest[0] if len(dest) > 0 else None
+
+        page = query.get("page", [])
+        page = page[0] if len(page) > 0 else None
+
+        comment = dest or page
+
+        candidates = [citation for citation in citations
+                      if citation_matches(citation, parsed.path) ]
+        if len(candidates) == 0:
+            print("NOT FOUND")
+        elif len(candidates) == 1:
+            if comment is not None:
+                print("\\cite[" + comment + "]{" + candidates[0]["ID"] + "}",end ="")
+            else:
+                print("\\cite{" + candidates[0]["ID"] + "}", end="")
+
+            with open(kl_file, "a") as f:
+                f.write(create_kl_citation(candidates[0], pasted_url, dest,
+                    page))
+        else:
+            print("TOO MANY CANDIDATES")
 
 
 if __name__ == '__main__':
     mode = sys.argv[1]
-
     if mode == "open":
         open_citation()
     elif mode == "create":
         create_citation()
-    elif mode == "insert":
-        insert_citation()
     else:
         exit(1)
